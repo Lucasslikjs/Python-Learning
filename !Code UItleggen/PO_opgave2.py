6567bbd1d0c4a76d2fd48fa7d072321614b83d07
Lucas van der Slik <72087463+Lucasslikjs@users.noreply.github.com>
2020-12-26 17:51:58 +0100

    Update PO_opgave2.py

--- !Code UItleggen/PO_opgave2.py
+++ !Code UItleggen/PO_opgave2.py
@@ -1,15 +1,14 @@
-aantalGetallen = int(input("Hoeveel getallen wil je invoeren?")) #Er word gevraagd hoeveel getal de gebruiker wil invoeren
-                                                                 #Door middel van de funtie "input"
-totaal = 0 #Deze regel maakt een variable aan genaamd "totaal"
-teller = 0 #Deze regel maakt een variable aan genaamd "telles"
+aantalGetallen = int(input("Hoeveel getallen wil je invoeren?"))
+totaal = 0 
+teller = 0 
 
-while teller < aantalGetallen: #Als de variable "teller" kleiner is dan "aantalGetalen" gebeurt het volgede:
-    getal = float(input("Voer getal in")) #Vraag de gebruiken om een getal in te vullen
-    totaal += getal #Voegt het getal toe aan de variable "totaal"  
-    teller += 1 #Verhoog de variable "teller" met 1
+while teller < aantalGetallen: 
+    getal = float(input("Voer getal in")) 
+    totaal += getal 
+    teller += 1 
     
-    gemiddelde = totaal / aantalGetallen #Deelt de som "totaal" door "aantalGetallen"
-    print("Het gemiddelde is: " + str(gemiddelde)) #laat het gemiddelde zien doormiddel van de functie print
+    gemiddelde = totaal / aantalGetallen "
+    print("Het gemiddelde is: " + str(gemiddelde)) 
 
 
 
